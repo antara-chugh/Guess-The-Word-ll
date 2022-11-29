5de69da791120dee813907ca615fe2643c6e9fd0
Antara <antarachugh@g.ucla.edu>
2022-11-28 21:25:04 -0800

    Initial Commit

---
+++ Guess The Word'll/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Guess The Word'll
+//
+//  Created by Antara Chugh on 11/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
